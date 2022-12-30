9ed3e963b68df18117ffe6636e8551c592868549
Will <will@containerlabs.io>
2022-12-30 11:45:28 -0800

    update module

--- core-services/main.tf
+++ core-services/main.tf
@@ -1,7 +1,10 @@
-
 module "core_services" {
-  source      = "../../terraform-modules/gcp-gke-core-services"
-  md_metadata = var.md_metadata
+  source                      = "github.com/massdriver-cloud/terraform-modules//gcp-gke-core-services"
+  md_metadata                 = var.md_metadata
+  kubernetes_cluster_artifact = local.kubernetes_cluster_artifact
+  enable_ingress              = var.core_services.enable_ingress
+  cloud_dns_managed_zones     = var.core_services.cloud_dns_managed_zones
+
   logging = {
     opensearch = {
       enabled             = var.observability.logging.destination == "opensearch"
@@ -9,9 +12,9 @@ module "core_services" {
       retention_days      = try(var.observability.logging.opensearch.retention_days, 7)
     }
   }
-  vpc_grn                       = var.subnetwork.data.infrastructure.gcp_global_network_grn
-  gcp_project_id                = var.gcp_authentication.data.project_id
-  gcp_region                    = var.subnetwork.specs.gcp.region
-  cloud_dns_managed_zones       = var.core_services.cloud_dns_managed_zones
-  control_plane_ipv4_cidr_block = var.cluster_networking.master_ipv4_cidr_block
+
+  gcp_config = {
+    project_id = var.gcp_authentication.data.project_id
+    region     = var.subnetwork.specs.gcp.region
+  }
 }
