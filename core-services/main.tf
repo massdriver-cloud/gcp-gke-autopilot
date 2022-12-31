86ef5b39c11f7305b934e56ac5b37e513c955e1e
Will <will@containerlabs.io>
2022-12-31 11:11:13 -0800

    core services rinse

--- core-services/main.tf
+++ core-services/main.tf
@@ -1,5 +1,6 @@
 module "core_services" {
-  source                      = "github.com/massdriver-cloud/terraform-modules//gcp-gke-core-services"
+  # source                      = "github.com/massdriver-cloud/terraform-modules//gcp-gke-core-services?ref=8d71513"
+  source                      = "../../../terraform-modules/gcp-gke-core-services"
   md_metadata                 = var.md_metadata
   kubernetes_cluster_artifact = local.kubernetes_cluster_artifact
   enable_ingress              = var.core_services.enable_ingress
