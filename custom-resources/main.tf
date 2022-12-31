6e4d13c8d9738cb51cff314eed4f4d2fafc83e1a
Will <will@containerlabs.io>
2022-12-31 11:39:57 -0800

    rename module

--- custom-resources/main.tf
+++ custom-resources/main.tf
@@ -1,4 +1,4 @@
-module "core_services" {
+module "custom_resources" {
   # source                      = "github.com/massdriver-cloud/terraform-modules//gcp-gke-core-services?ref=8d71513"
   source                  = "../../../tf-modules/terraform-modules/gcp-gke-custom-resources"
   cloud_dns_managed_zones = var.core_services.cloud_dns_managed_zones
