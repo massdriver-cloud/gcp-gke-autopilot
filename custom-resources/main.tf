c01de670cb804f8197e33e8bc22c08ac35ddd6f0
Christopher Hill <chris@massdriver.cloud>
2023-01-15 23:14:06 -0700

    update core-services module to wait for service account token (#6)

--- custom-resources/main.tf
+++ custom-resources/main.tf
@@ -1,5 +1,5 @@
 module "custom_resources" {
-  source                  = "github.com/massdriver-cloud/terraform-modules//gcp-gke-custom-resources?ref=b3f3449"
+  source                  = "github.com/massdriver-cloud/terraform-modules//gcp-gke-custom-resources?ref=1b37297"
   cloud_dns_managed_zones = var.core_services.cloud_dns_managed_zones
 
   gcp_config = {
