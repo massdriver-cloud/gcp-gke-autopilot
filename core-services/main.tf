1906dc8ad8f89e9f90cb6a0d287ed2cb229eb620
chrisghill <chris@massdriver.cloud>
2023-01-16 01:37:31 -0700

    module ref

--- core-services/main.tf
+++ core-services/main.tf
@@ -1,5 +1,5 @@
 module "core_services" {
-  source                      = "github.com/massdriver-cloud/terraform-modules//gcp-gke-core-services?ref=9bf5901"
+  source                      = "github.com/massdriver-cloud/terraform-modules//gcp-gke-core-services?ref=a63968d"
   md_metadata                 = var.md_metadata
   kubernetes_cluster_artifact = local.kubernetes_cluster_artifact
   enable_ingress              = var.core_services.enable_ingress
